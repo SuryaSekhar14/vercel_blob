41d3d78871ea01aec67eab42da1506a8d3b2ca88
SuryaSekhar14 <suryasekhardatta22@gmail.com>
2024-06-07 00:03:12 +0530

    bug squash in getting token from env

--- vercel_blob/blob_store.py
+++ vercel_blob/blob_store.py
@@ -16,11 +16,11 @@ def _get_auth_token_from_env() -> str:
 
     if not token:
         raise Exception('BLOB_READ_WRITE_TOKEN environment variable not set')
-    
+
     return token
 
 
-def _request_factory(url: str, method: str, backoff_factor: int = 0.3, status_forcelist: list = [502, 503, 504], timeout: int = 10, **kwargs) -> requests.Response:
+def _request_factory(url: str, method: str, backoff_factor: int = 0.5, status_forcelist: list = [502, 503, 504], timeout: int = 10, **kwargs) -> requests.Response:
     for attempt in range(1, _MAX_RETRY_REQUEST_RETRIES + 1):
         try:
             response = requests.request(method, url, timeout=timeout, **kwargs)
@@ -43,26 +43,30 @@ def _response_handler(resp: requests.Response) -> dict:
 
 def list(opts: dict = {}) -> dict:
     try:
-        _tkn = opts.get("token")
+        _tkn = opts['token']
     except KeyError:
         _tkn = _get_auth_token_from_env()
 
     headers = {
         "authorization": f'Bearer {_tkn}',
+    }
+
+    params = {
         "limit": opts.get('limit', str(_PAGINATED_LIST_SIZE)),
     }
 
     if opts.get('prefix'):
-        headers['prefix'] = opts['prefix']
+        params['prefix'] = opts['prefix']
     if opts.get('cursor'):
-        headers['cursor'] = opts['cursor']
+        params['cursor'] = opts['cursor']
     if opts.get('mode'):
-        headers['mode'] = opts['mode']
+        params['mode'] = opts['mode']
 
-    if _DEBUG == True: print(headers)
+    if _DEBUG: print(headers)
     resp = _request_factory(
         f"{_VERCEL_BLOB_API_BASE_URL}",
         'GET',
+        params=params,
         headers=headers,
     )
 
@@ -71,7 +75,7 @@ def list(opts: dict = {}) -> dict:
 
 def put(path: str, data: bytes, opts: dict = {}) -> dict:
     try:
-        _tkn = opts.get("token")
+        _tkn = opts['token']
     except KeyError:
         _tkn = _get_auth_token_from_env()
 
@@ -86,7 +90,7 @@ def put(path: str, data: bytes, opts: dict = {}) -> dict:
     if opts.get('addRandomSuffix') == "false":
         headers['x-add-random-suffix'] = "false"
 
-    if _DEBUG == True: print(headers)
+    if _DEBUG: print(headers)
     resp = _request_factory(
         f"{_VERCEL_BLOB_API_BASE_URL}/{path}",
         'PUT',
@@ -99,7 +103,7 @@ def put(path: str, data: bytes, opts: dict = {}) -> dict:
 
 def head(url: str, opts: dict = {}) -> dict:
     try:
-        _tkn = opts.get("token")
+        _tkn = opts['token']
     except KeyError:
         _tkn = _get_auth_token_from_env()
 
@@ -108,7 +112,7 @@ def head(url: str, opts: dict = {}) -> dict:
         "x-api-version": _API_VERSION,
     }
 
-    if _DEBUG == True: print(headers)
+    if _DEBUG: print(headers)
     resp = _request_factory(
         f"{_VERCEL_BLOB_API_BASE_URL}/?url={url}",
         'GET',
@@ -120,7 +124,7 @@ def head(url: str, opts: dict = {}) -> dict:
 
 def delete(url: any, opts: dict = {}) -> dict:
     try:
-        _tkn = opts.get("token")
+        _tkn = opts['token']
     except KeyError:
         _tkn = _get_auth_token_from_env()
 
@@ -130,7 +134,7 @@ def delete(url: any, opts: dict = {}) -> dict:
     }
 
     if type(url) == type("") or type(url) == type([]):
-        if _DEBUG == True: print(headers)
+        if _DEBUG: print(headers)
         resp = _request_factory(
             f"{_VERCEL_BLOB_API_BASE_URL}/delete",
             'POST',
