b173d93af958a5871ea4c3369942e4a6d08f9ea2
SuryaSekhar14 <suryasekhardatta22@gmail.com>
2024-06-06 22:31:22 +0530

    rename

--- vercel_blob/blob_store.py
+++ vercel_blob/blob_store.py
@@ -1,11 +1,13 @@
 import requests
 import os
+import time
 
 
 _VERCEL_BLOB_API_BASE_URL = 'https://blob.vercel-storage.com'
 _API_VERSION = '7'
 _PAGINATED_LIST_SIZE = 1000
 _DEFAULT_CACHE_AGE = '31536000'
+_MAX_RETRY_REQUEST_RETRIES = 3
 _DEBUG = os.environ.get('DEBUG', False)
 
 
@@ -18,16 +20,35 @@ def _get_auth_token_from_env() -> str:
     return token
 
 
+def _request_factory(url: str, method: str, backoff_factor: int = 0.3, status_forcelist: list = [502, 503, 504], timeout: int = 10, **kwargs) -> requests.Response:
+    for attempt in range(1, _MAX_RETRY_REQUEST_RETRIES + 1):
+        try:
+            response = requests.request(method, url, timeout=timeout, **kwargs)
+            if response.status_code not in status_forcelist:
+                return response
+        except requests.exceptions.RequestException as e:
+            print(f"Request failed on attempt {attempt} ({e})")
+            time.sleep(backoff_factor * attempt) 
+    return None
+
+
 def _response_handler(resp: requests.Response) -> dict:
-    if resp.status_code == 200:
+    if resp is None:
+        raise Exception("Request failed after retries. Please try again.")
+    elif resp.status_code == 200:
         return resp.json()
     else:
         raise Exception(f"An error occoured: {resp.json()}")
 
 
 def list(opts: dict = {}) -> dict:
+    try:
+        _tkn = opts.get("token")
+    except KeyError:
+        _tkn = _get_auth_token_from_env()
+
     headers = {
-        "authorization": f'Bearer {opts.get("token", _get_auth_token_from_env())}',
+        "authorization": f'Bearer {_tkn}',
         "limit": opts.get('limit', str(_PAGINATED_LIST_SIZE)),
     }
 
@@ -39,8 +60,9 @@ def list(opts: dict = {}) -> dict:
         headers['mode'] = opts['mode']
 
     if _DEBUG == True: print(headers)
-    resp = requests.get(
+    resp = _request_factory(
         f"{_VERCEL_BLOB_API_BASE_URL}",
+        'GET',
         headers=headers,
     )
 
@@ -48,9 +70,14 @@ def list(opts: dict = {}) -> dict:
 
 
 def put(path: str, data: bytes, opts: dict = {}) -> dict:
+    try:
+        _tkn = opts.get("token")
+    except KeyError:
+        _tkn = _get_auth_token_from_env()
+
     headers = {
         "access": "public",  # Support for private is not yet there, according to Vercel docs at time of writing this code
-        "authorization": f'Bearer {opts.get("token", _get_auth_token_from_env())}',
+        "authorization": f'Bearer {_tkn}',
         "x-api-version": _API_VERSION,
         "Content-Type": "application/octet-stream",
         "cacheControlMaxAge": opts.get('cacheControlMaxAge', _DEFAULT_CACHE_AGE),
@@ -60,24 +87,31 @@ def put(path: str, data: bytes, opts: dict = {}) -> dict:
         headers['x-add-random-suffix'] = "false"
 
     if _DEBUG == True: print(headers)
-    resp = requests.put(
+    resp = _request_factory(
         f"{_VERCEL_BLOB_API_BASE_URL}/{path}",
-        data=data,
+        'PUT',
         headers=headers,
+        data=data,
     )
 
     return _response_handler(resp)
 
 
 def head(url: str, opts: dict = {}) -> dict:
+    try:
+        _tkn = opts.get("token")
+    except KeyError:
+        _tkn = _get_auth_token_from_env()
+
     headers = {
-        "authorization": f'Bearer {opts.get("token", _get_auth_token_from_env())}',
+        "authorization": f'Bearer {_tkn}',
         "x-api-version": _API_VERSION,
     }
 
     if _DEBUG == True: print(headers)
-    resp = requests.get(
+    resp = _request_factory(
         f"{_VERCEL_BLOB_API_BASE_URL}/?url={url}",
+        'GET',
         headers=headers,
     )
 
@@ -85,17 +119,23 @@ def head(url: str, opts: dict = {}) -> dict:
 
 
 def delete(url: any, opts: dict = {}) -> dict:
+    try:
+        _tkn = opts.get("token")
+    except KeyError:
+        _tkn = _get_auth_token_from_env()
+
     headers = {
-        "authorization": f'Bearer {opts.get("token", _get_auth_token_from_env())}',
+        "authorization": f'Bearer {_tkn}',
         "x-api-version": _API_VERSION,
     }
 
     if type(url) == type("") or type(url) == type([]):
         if _DEBUG == True: print(headers)
-        resp = requests.post(
+        resp = _request_factory(
             f"{_VERCEL_BLOB_API_BASE_URL}/delete",
-            json={"urls": [url] if isinstance(url, str) else url},
+            'POST',
             headers=headers,
+            json={"urls": [url] if isinstance(url, str) else url},
         )
         return _response_handler(resp)
     else:
